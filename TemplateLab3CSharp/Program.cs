8556f82405d1e483b3445735ff092c7bd1d15eb6
Haras-Oleh <90175358+Haras-Oleh@users.noreply.github.com>
2022-11-20 23:51:46 +0200

    Update Program.cs

--- TemplateLab3CSharp/Program.cs
+++ TemplateLab3CSharp/Program.cs
@@ -1,97 +1,208 @@
-﻿// See https://aka.ms/new-console-template for more information
-using MyTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zavdannia1;
+using zavdannia2;
 
-/// <summary>
-///  Top-level statements 
-///  Код програми (оператори)  вищого рівня
-/// </summary>
-/// 
-Console.WriteLine("Hello, World!");
-//
-//  Приклад коду
-//
+//https://github.com/Haras-Oleh/csharp_lab3
 
-Animal[] animals = new Animal[4];
-animals[0] = new Animal();
-animals[0].Name = "Bizon"; animals[0].WWeight=100;
-Console.WriteLine(" my animal " + animals[0].ToString());
-animals[1] = new Animal(100,10,"Cow");
-animals[2] = new Animal(102, 12, "Cow Big");
-animals[3] = new Animal(100, 10, "Zebra");
-
-for(int i = 0; i < 4; i++)
+namespace zavdannia1 
 {
-    Console.WriteLine("  animal " + i + " " + animals[i].ToString());
+    public class Romb
+    {
+protected int a { get; set; }
+protected int d1 { get; set; }
+protected string c { get; }
+
+public Romb(int a_, int d1_, string c_)
+{
+if (a_ > 0)
+{
+a = a_;
+}
+if (d1_ > 0)
+{
+d1 = d1_;
+}
+c = c_;
 }
 
-Big b = new Big();
-Console.WriteLine(" Big" + b +"   "+ b.ToString() + "  " + b.Name);
-
-Console.WriteLine(" Task 2");
-Task2.Run();
-     
-
-/// <summary>
-///  Закічення  іструкцій верхнього рівня
-/// Top-level statements must precede namespace and type declarations.
-/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
-///   
-/// </summary>
-class Big
-{
-    private string name =" First init";
-    public Big()
-    {
-        name = "NoName";
-    }
-    public Big(string name)
-    {
-        this.name = name;
-        
-    }
-    public string Name { 
-        get { return name; } 
-        set { name = value; }
-    }
-
-};
-namespace MyTask
-{
-    static class Task2
+        public void Otrimatu_Storony_ta_Diagonal()
         {
-       public  static void Run()
-        {
-            Drv d = new Drv("RRR");
-            Console.WriteLine(" rrr  " + d.ToString());
+        Console.WriteLine($"Сторона A = {a}");
+        Console.WriteLine($"Дiагональ D1 = {d1}");
         }
-        }
-    class Base
-    {
-        string NameBase;
-        protected string any_Inform;
-        int type = 0;
-        public int numobj = 0;
-        public Base() {
-            NameBase = "Base";
-            any_Inform = "";
 
+        public void Otrimatu_Perimetr()
+        {
+        int perimetr = a + a + a + a;
+        Console.WriteLine($"Периметр дорiвнює: {perimetr}");
         }
+
+        public void Otrimatu_Ploschu()
+        {
+        int ploscha = (a * d1)/2;
+        Console.WriteLine($"Площа дорiвнює: {ploscha}");
+        }
+
+        public void Chu_E_Kvadratom()
+        {
+        if (d1 == a * Math.Sqrt(2))
+        {
+        Console.WriteLine("Даний Ромб є квадратом!");
+        }
+        if (d1 != a * Math.Sqrt(2))
+        {
+        Console.WriteLine("Даний Ромб НЕ є квадратом!");
+        }
+        }
+        }
+        }
+
+namespace zavdannia2
+{
+    public class Detal
+        {
+        protected string  virobnuk{ get; set; }
+
+        public Detal(string virobnuk)
+        {
+        this.virobnuk = virobnuk;
+        }
+
         public void Show()
         {
-            Console.WriteLine(" Show Base " + NameBase);
+        Console.WriteLine("Список iнформацiї про дану деталь:");
+        Console.WriteLine($"виробник: {virobnuk}");
         }
-    };
+        }
 
-    class Drv :  Base
-    {
-        Drv()
+    public class Mehanizm : Detal
         {
-            numobj = 10;
-        }
-       public Drv(string any_Inform)
+        protected string kraina { get; set; }
+
+        public Mehanizm(string virobnuk, string kraina) : base(virobnuk)
         {
-            numobj = 11;
-            this.any_Inform = any_Inform;
+        this.virobnuk = virobnuk;
+        this.kraina = kraina;
         }
-    }
-}
+
+        public void Show()
+        {
+        Console.WriteLine("Список iнформацiї про даний механiзм:");
+        Console.WriteLine($"Виробник: {this.virobnuk}");
+        Console.WriteLine($"Країна: {this.kraina}");
+        }
+        }
+
+    public class Vurib : Mehanizm
+        {
+        public Vurib(string virobnuk, string kraina) : base(virobnuk, kraina)
+        {
+        this.virobnuk = virobnuk;
+        this.kraina = kraina;
+        }
+
+        public void Show()
+        {
+        Console.WriteLine("Список iнформацiї про даний вирiб:");
+        Console.WriteLine($"Виробник: {this.virobnuk}");
+        Console.WriteLine($"Країна: {this.kraina}");
+        }
+        }
+
+    public class Vuzol : Mehanizm
+        {
+        protected string posada { get; set; }
+
+        public Vuzol(string virobnuk, string kraina) : base(virobnuk,kraina)
+        {
+        this.posada = posada;
+        this.virobnuk = virobnuk;
+        this.kraina = kraina;
+        }
+
+        public void Show()
+        {
+        Console.WriteLine("Список iнформацiї про даний вузол:");
+        Console.WriteLine($"Виробник : {this.virobnuk}");
+        Console.WriteLine($"Країна: {this.kraina}");
+        }
+        }
+        }
+
+namespace laboratorna_robota3
+{
+        internal class Program
+        {
+        static void Main(string[] args)
+        {
+        int vubir = 0;
+        do
+        {
+        Console.Write("Оберiть,будь ласка,одне iз двох завдань даної лабораторної роботи,яке Ви хочете перевiрити?: ");
+        vubir = int.Parse(Console.ReadLine());
+        } while (vubir != 1 && vubir != 2);
+
+        switch (vubir)
+        {
+        case 1:
+        Console.WriteLine("Хiд роботи першого завдання:");
+        {
+        Console.Write("Введiть,будь ласка, розмiр сторони а: ");
+        int a = int.Parse(Console.ReadLine());
+		
+        Console.Write("Введiть,будь ласка, розмiр дiагоналi d1: ");
+        int d1 = int.Parse(Console.ReadLine());
+		
+        Console.Write("Введiть,будь ласка,колiр ромба : ");
+        string color = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.WriteLine("Розмiрностi Вашого ромба:");
+        Romb A = new Romb(a, d1, color);
+        A.Otrimatu_Storony_ta_Diagonal();
+        Console.WriteLine();
+
+        Console.WriteLine("Периметр Вашого ромба:");
+        A.Otrimatu_Perimetr();
+        Console.WriteLine();
+
+        Console.WriteLine("Площа Вашого ромба:");
+        A.Otrimatu_Ploschu();
+        Console.WriteLine();
+
+        Console.WriteLine("Перевiрка чи є Ваший ромб квадратом:");
+        A.Chu_E_Kvadratom();
+        Console.WriteLine();
+        }
+        break;
+        case 2:
+        Console.WriteLine("Хiд роботи другого завдання:");
+        {
+        Detal Detal = new Detal("Автомобiля");
+        Detal.Show();
+        Console.WriteLine();
+
+        Mehanizm Mehanizm = new Mehanizm("Volkswagen", "Нiмеччина");
+        Mehanizm.Show();
+        Console.WriteLine();
+
+        Vurib Vurib = new Vurib("Bosch", "Австрiя");
+        Vurib.Show();
+        Console.WriteLine();
+
+        Vuzol vuzol = new Vuzol("Davide Campari-Milano", "Iталiя");
+        vuzol.Show();
+        Console.WriteLine();
+        }
+        break;
+        default:
+        Console.WriteLine("Помилка!");
+        break;
+        }
+        }
+        }
+        }
